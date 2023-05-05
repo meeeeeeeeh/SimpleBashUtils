fb1d9c914323c1cdcf3592487033e83b2b4cb220
Endehar Harkonnen <endeharh@il-g1.msk.21-school.ru>
2023-05-05 20:09:20 +0300

    q2

--- src/grep/grep_new.c
+++ src/grep/grep_new.c
@@ -3,13 +3,13 @@
 #include <regex.h>
 #include <string.h>
 
-void grep(char* template, char* fp);
-void grep_i(char* template, char* fp);
-void grep_c(char* template, char* fp);
-void grep_v(char* template, char* fp);
-void grep_l(char* template, char* fp);
+//optind инициализируется как 1. (!!)
+//Если getopt возвращает -1 (флагов больше нет) 
+//optind становится индексом первого элемента коммандной строки который не является опцией
 
-typedef struct flag {
+//void print_lines(char * fp, int files, char flag, char* templ);
+
+typedef struct flag{
     int e;
     char template[300];
     int i;
@@ -20,29 +20,22 @@ typedef struct flag {
     int h;
     int s;
     int f;
-    int o;
     char file[300]; 
+    int o;
+    int is_flag;
 } flag;
 
+void print_lines(char * fp, int files, flag f);
 
 int main(int argc, char* argv[]) {
-    int opt = 0;
-
     flag f = {}; //делает всем значениям структуры значение 0
     f.template[0] = '\0';
     f.file[0] = '\0';
-
-    //Допустимые флаги, если есть :, то после флага должен быть шаблон(то что после флага считается шаблоном)
-    //шаблон заносится в переменную getopt
-    //Если после флага ничего не стоит возращается NULL - для обработки ошибок
-
-    // optind  это индекс аргумента строки который не шаблон и не флаг
-
-    // printf("before argv: ");
-    // for (int i = 0; i < argc; i++) printf("%s ", argv[i]);
-    // printf("\n");
+    int lots_files = 0;
+    int opt = 0;
 
     while((opt = getopt(argc, argv, "e:ivclnhsf:o")) != -1) {
+        f.is_flag = 1; //какой-то флаг есть
         if (opt == 'e') {
             f.e = 1;  
             strcat(f.template, optarg); // "str1|str2|1231|"
@@ -50,8 +43,7 @@ int main(int argc, char* argv[]) {
         }
         if (opt == 'f') {
             f.f = 1;  
-            strcat(f.file, optarg); // "str1|str2|1231|"
-            strcat(f.file, " ");
+            strcat(f.file, optarg); // из файла берутся шаблоны
         }
         else if (opt == 'i') f.i = 1;
         else if (opt == 'v') f.v = 1;
@@ -62,6 +54,7 @@ int main(int argc, char* argv[]) {
         else if (opt == 's') f.s = 1;
         else if (opt == 'o') f.o = 1;
     }
+     
     f.template[strlen(f.template) - 1] = '\0'; // "str1|str2|1231"
 
     if (f.e == 0) {
@@ -69,214 +62,87 @@ int main(int argc, char* argv[]) {
         optind++;
     }
 
-   /*printf("\ntemplates: %s\n\n", f.template);
-
-    printf("files: ");
-    for (int i = optind; i < argc; i++) printf("%s ", argv[i]);
-    printf("\n\n");*/
+    if (optind == argc - 1) {
+        lots_files = 1;
+    }
 
     for (int i = optind; i < argc; i++) {
-        FILE* file;
-        
-        if ((file = fopen(argv[i], "r")) != NULL) {
-            regex_t re;
-            char buffer[500];
-            int match;
 
-            while (fgets(buffer, 500, file) != NULL) {
-
-                regcomp(&re, f.template, REG_EXTENDED);//0-sucses
-                match = regexec(&re, buffer, 0, NULL, 0);//0-match
-                
-                if (!match) {
-                    printf("%s", buffer);
-                }
-                regfree(&re);
-            }
-            fclose(file);
-        } 
-        else {
-            fprintf(stderr, "There's no such file \"%s\"\n", argv[i]);
+        if (f.e == 1 || f.is_flag == 0) {
+            print_lines(argv[i], lots_files, f);
         }
-        
-    }
-
-    /*printf("after argv: ");
-    for (int i = 0; i < argc; i++) printf("%s ", argv[i]);
-    printf("\n");
-
-    printf("%d\n", optind);
-    printf("%s\n", optarg);
-   
-    
-    if (no_flags) {
-        for (int i = 2; i < argc; i++) {
-            grep(argv[1], argv[i]);
+        else if (f.is_flag == 0){
+            print_lines(argv[i], lots_files, f);
+        }
+        else if (f.i == 1){
+            print_lines(argv[i], lots_files, f);
+        }
+        else if (f.v == 1){
+            //no enter between files. Check output(!)
+            print_lines(argv[i], lots_files, f);
+        }
+        else if (f.h == 1){
+            print_lines(argv[i], lots_files, f);
+        }
+        else if (f.s == 1){
+            print_lines(argv[i], lots_files, f);
         }
     }
-    else {
-        for (int k = optind; k < argc; k++) {
-            for (int i=0; i < count; i++) {
-                if (flags[i] == 'e') {
-                    printf("%s\n", argv[k]);
-                    grep(optarg, argv[k]); 
-                }
-                
-                if (flags[i] == 'i') {
-                    printf("%s\n", argv[k]);
-                    grep_i(optarg, argv[k]);
-                }
 
-                if (flags[i] == 'c') {
-                    printf("%s\n", argv[k]);
-                    grep_c(optarg, argv[k]);
-                }
-
-                if (flags[i] == 'v') {
-                    printf("%s\n", argv[k]);
-                    grep_v(optarg, argv[k]); 
-                }
-                if (flags[i] == 'l') {
-                    printf("%s\n", argv[k]);
-                    grep_l(optarg, argv[k]); 
-                }
-            }
-        }
-        
-    }*/
-    
-    
     return 0;
 }
 
-/*void grep_l(char* template, char* fp) {
-    regex_t re;
+void print_lines(char * fp, int files, flag f) {
     FILE* file;
+    regex_t re;
     char buffer[500];
     int match;
-    int print_file = 0;
 
     if ((file = fopen(fp, "r")) != NULL) {
-        while (fgets(buffer, 500, file) != NULL) {
 
-            regcomp(&re, template, REG_EXTENDED);
-            //0-sucsess
-            match = regexec(&re, buffer, 0, NULL, 0);
-            //0-match
-            if (!match) {
-                print_file = 1;
+        while (fgets(buffer, 500, file) != NULL) {
+            
+            if (f.i) {
+                regcomp(&re, f.template, REG_ICASE);
             }
+            else {
+                regcomp(&re, f.template, REG_EXTENDED);//0-sucses
+            }
+            match = regexec(&re, buffer, 0, NULL, 0);//0-match
+
+            if (f.v) {
+                if (match != 0) {
+                    if (files){
+                        printf("%s", buffer);
+                    }
+                    else {
+                        printf("%s:%s", fp, buffer);
+                    }
+                }
+            }
+            if (f.e || f.i || (!f.is_flag) || f.s) {
+                if (!match) {
+                    if (files){
+                        printf("%s", buffer);
+                    }
+                    else {
+                        printf("%s:%s", fp, buffer);
+                    }
+                }
+            }
+            if (f.h) {
+                if (match != 0) {
+                    printf("%s", buffer);
+                }
+            }
+
             regfree(&re);
         }
         fclose(file);
-        if(print_file) {
-            printf("%s\n", fp);
-        }
-    } 
-    else {
-        printf("There's no such file \"%s\"\n", fp);
     }
+
+    else {
+        if (!f.s) fprintf(stderr, "There's no such file \"%s\"\n", fp);
+    }
+    
 }
-
-void grep_v(char* template, char* fp) {
-    regex_t re;
-    FILE* file;
-    char buffer[500];
-    int match;
-
-    if ((file = fopen(fp, "r")) != NULL) {
-        while (fgets(buffer, 500, file) != NULL) {
-
-            regcomp(&re, template, REG_EXTENDED);
-            //0-sucsess
-            match = regexec(&re, buffer, 0, NULL, 0);
-            //0-match
-            if (match != 0) {
-                printf("%s", buffer);
-            }
-            regfree(&re);
-        }
-        fclose(file);
-    } 
-    else {
-        printf("There's no such file \"%s\"\n", fp);
-    }
-}
-
-void grep_c(char* template, char* fp) {
-    regex_t re;
-    FILE* file;
-    char buffer[500];
-    int match;
-    int count = 0;
-
-    if ((file = fopen(fp, "r")) != NULL) {
-        while (fgets(buffer, 500, file) != NULL) {
-
-            regcomp(&re, template, REG_EXTENDED);
-            //0-sucsess
-            match = regexec(&re, buffer, 0, NULL, 0);
-            //0-match
-            if (!match) {
-                count++;
-            }
-            regfree(&re);
-        }
-        printf("%d", count);
-        fclose(file);
-    } 
-    else {
-        printf("There's no such file \"%s\"\n", fp);
-    }
-}
-
-void grep(char* template, char* fp) {
-    regex_t re;
-    FILE* file;
-    char buffer[500];
-    int match;
-
-    if ((file = fopen(fp, "r")) != NULL) {
-        while (fgets(buffer, 500, file) != NULL) {
-
-            regcomp(&re, template, REG_EXTENDED);
-            //0-sucsess
-            match = regexec(&re, buffer, 0, NULL, 0);
-            //0-match
-            if (!match) {
-                printf("%s", buffer);
-            }
-            regfree(&re);
-        }
-        fclose(file);
-    } 
-    else {
-        printf("There's no such file \"%s\"\n", fp);
-    }
-}
-
-void grep_i(char* template, char* fp) {
-    regex_t re;
-    FILE* file;
-    char buffer[500];
-    int match;
-
-    if ((file = fopen(fp, "r")) != NULL) {
-        while (fgets(buffer, 500, file) != NULL) {
-
-            regcomp(&re, template, REG_ICASE);
-            //0-sucsess
-            match = regexec(&re, buffer, 0, NULL, 0);
-            //0-match
-            if (!match) {
-                printf("%s", buffer);
-            }
-
-            regfree(&re);
-        }
-        fclose(file);
-    } 
-    else {
-        printf("There's no such file \"%s\"\n", fp);
-    }
-}*/
